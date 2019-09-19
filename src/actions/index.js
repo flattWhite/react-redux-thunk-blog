5af722e76de2e2cd8ee12c0bc9c92b83945cd357
flattWhite <onlyforsocial01@gmail.com>
2019-09-19 15:24:39 -0400

    clean up the code with es2015 syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,8 @@
 //action creator, call
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-	return async function(dispatch, getState) {
-		//dispatch, getState -> can access and modify data in redux
-		const response = await jsonPlaceholder.get("/posts");
-		dispatch({ type: "FETCH_POSTS", payload: response });
-	};
+export const fetchPosts = () => async dispatch => {
+	//dispatch, getState -> can access and modify data in redux
+	const response = await jsonPlaceholder.get("/posts");
+	dispatch({ type: "FETCH_POSTS", payload: response });
 };
