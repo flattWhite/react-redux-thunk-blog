baacec948b3892fc60e1b9c37f4809715191a262
flattWhite <onlyforsocial01@gmail.com>
2019-09-20 15:59:08 -0400

    fix overfetching issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,18 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {};
+// return inner function (fetchPosts)
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	// await keyword => make sure that we successfully fetched a list o posts
+	// and dispatch the action before the next line of code
+	await dispatch(fetchPosts());
 
+	// Go through all posts and get unique userId
+	const userIds = _.uniq(_.map(getState().posts, "userId"));
+	userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+// dispatch function shows fetchPost inside redux thunk and get invoked with dispatch
 export const fetchPosts = () => async dispatch => {
 	//dispatch, getState -> can access and modify data in redux
 	const response = await jsonPlaceholder.get("/posts");
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions";
+import { fetchPostsAndUsers } from "../actions";
 import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
 	componentDidMount() {
-		this.props.fetchPosts();
+		this.props.fetchPostsAndUsers();
 	}
 
 	renderList() {
@@ -36,6 +36,6 @@ const mapStateToProps = state => {
 export default connect(
 	mapStateToProps,
 	{
-		fetchPosts
+		fetchPostsAndUsers
 	}
 )(PostList);
