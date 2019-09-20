85793bfdcb6f366e3e6a92f0eb7ae75077b24d97
flattWhite <onlyforsocial01@gmail.com>
2019-09-20 14:43:57 -0400

    Fix the issue of sending same fetch user request of the same user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 //action creator, call
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -7,8 +8,22 @@ export const fetchPosts = () => async dispatch => {
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => {
-	const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = id => dispatch => {
+// 	_fetchUser(id, dispatch);
+// };
 
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// private function
+const _fetchUser = _.memoize(async (id, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
 	dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
+
+// export const fetchUser = function(id) {
+// 	return _.memoize(async function(dispatch) {
+// 		const response = await jsonPlaceholder.get(`/users/${id}`);
+
+// 		dispatch({ type: "FETCH_USER", payload: response.data });
+// 	});
+// };
