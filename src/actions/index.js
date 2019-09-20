9d1d25b21e4e20f0f94bd659d513f7726c181754
flattWhite <onlyforsocial01@gmail.com>
2019-09-19 22:37:03 -0400

    created a reducer for fetch post action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,5 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async dispatch => {
 	//dispatch, getState -> can access and modify data in redux
 	const response = await jsonPlaceholder.get("/posts");
-	dispatch({ type: "FETCH_POSTS", payload: response });
+	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
