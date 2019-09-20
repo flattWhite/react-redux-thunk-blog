7ff4b991094b1f9470c1967f75d1d8441a6e3ae9
flattWhite <onlyforsocial01@gmail.com>
2019-09-20 14:16:55 -0400

    Find a single user and set it to props

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,17 +8,17 @@ class UserHeader extends React.Component {
 	}
 
 	render() {
-		const user = this.props.users.find(
-			user => user.id === this.props.userId
-		);
+		const { user } = this.props;
 		if (!user) return null;
 
 		return <div className="header">{user.name}</div>;
 	}
 }
 
-const mapStateToProps = state => {
-	return { users: state.users };
+// state from redux store
+// ownProps a reference to the props that are about to be sent into UserHeader component
+const mapStateToProps = (state, ownProps) => {
+	return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(
