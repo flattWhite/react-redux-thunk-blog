73848b5c3966c16a0f148218799f59146ef607cb
flattWhite <onlyforsocial01@gmail.com>
2019-09-20 15:17:47 -0400

    commet out the memoize version code

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,28 +2,43 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async dispatch => {};
+
 export const fetchPosts = () => async dispatch => {
 	//dispatch, getState -> can access and modify data in redux
 	const response = await jsonPlaceholder.get("/posts");
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+/*
+export const fetchPosts = () => {
+	return async dispatch => {
+		//dispatch, getState -> can access and modify data in redux
+		const response = await jsonPlaceholder.get("/posts");
+		dispatch({ type: "FETCH_POSTS", payload: response.data });
+	};
+};
+*/
 
-// export const fetchUser = id => dispatch => {
-// 	_fetchUser(id, dispatch);
-// };
-
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-// private function
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
+
 	dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
 
-// export const fetchUser = function(id) {
-// 	return _.memoize(async function(dispatch) {
-// 		const response = await jsonPlaceholder.get(`/users/${id}`);
+/************************memoize version************************/
 
-// 		dispatch({ type: "FETCH_USER", payload: response.data });
-// 	});
-// };
+/* 
+export const fetchUser = id => dispatch => {
+ 	_fetchUser(id, dispatch);
+ };
+*/
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// // private function
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+// 	dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+/************************memoize version************************/
